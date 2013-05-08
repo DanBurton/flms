11bbb9a70090ace3c81e927864d468626fa181a4
Dan Burton <dan.burton@originate.com>
2013-05-08 14:20:28 -0700

    tweaks to image layer display, still not quite right

--- app/view_objects/flms/image_layer_view_object.rb
+++ app/view_objects/flms/image_layer_view_object.rb
@@ -5,14 +5,20 @@ module Flms
       @layer.image.url
     end
 
-    def attributes(scroll_offset = 0)
-      attributes = keyframe_data_hash(scroll_offset)
-      attributes[:id] = @layer.name
-      attributes[:style] += """
+    def background_style
+      """
         background-image: url(#{src});
         background-size: #{@layer.image_display_mode};
         background-repeat: no-repeat;
+        width: 100%;
+        height: 100%;
       """
+    end
+
+    def attributes(scroll_offset = 0)
+      attributes = keyframe_data_hash(scroll_offset)
+      attributes[:id] = @layer.name
+      # attributes[:style] += background_style
       attributes
     end
 
